8bec378f7cca94f0ab0f57afa0d980120576fa89
CodeJoker24 <damilaremishael@gmail.com>
2026-06-08 03:47:08 -0700

    added cors origin

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*", 
+   origin: ["http://localhost:5173", "https://talkio-frontend.vercel.app"], 
     methods: ["GET", "POST"]
   }
 });
